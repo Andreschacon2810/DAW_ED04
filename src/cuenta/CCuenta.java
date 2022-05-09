03802c5a5df341d89789224781b1a22ceaad148c
Andres <jeeffersonchacon2810@gmail.com>
2022-05-09 23:30:26 +0100

    * Cambiamos el nombre de la variable de CCuenta miCuenta por cuenta1. * Creamos el método operativa_cuenta para todas las operaciones del main. * Asignamos 2 párametros a el método creado anteriormente. * Encapsulamos los atributos de la clase CCuenta * No hay ningún cambio al ejecutar el programa. * Se realizó el JavaDoc

--- src/cuenta/CCuenta.java
+++ src/cuenta/CCuenta.java
@@ -9,27 +9,56 @@ public class CCuenta {
     public CCuenta() {
     }
 
+    /**
+     * Constructor parámetrizado
+     * @param nom parámetro para ingresar un nombre a el constructor del objeto
+     * @param cue parámetro para asignar un numero de cuenta
+     * @param sal Parámetro para ingresar el saldo de la cuenta
+     * @param tipo
+     */
     public CCuenta(String nom, String cue, double sal, double tipo) {
         setNombre(nom);
         setCuenta(cue);
         setSaldo(sal);
     }
 
+    /**
+     * Metódo para asignar nombre
+     * @param nom parámetro para asignar un nombre
+     */
     public void asignarNombre(String nom) {
         setNombre(nom);
     }
 
+    /**
+     * Metódo obtener nombre
+     * @return devulve el getter del nombre
+     */
     public String obtenerNombre() {
         return getNombre();
     }
 
+    /**
+     * Metodo estado de tipo double
+     * @return devuelve el saldo actual
+     */
     public double estado() {
         return getSaldo();
     }
 
+    /**
+     * Metodo obtener cuenta
+     * @return devulve el numero de cuenta
+     */
     public String obtenerCuenta() {
         return getCuenta();
     }
+
+    /**
+     * Metodo ingresar
+     * @param cantidad parametro que se utiliza para evaluar dentro del metodo
+     * @throws Exception si se cumple una condición dada dentro del metodo, mostrará un mensaje por pantalla.
+     */
 
     public void ingresar(double cantidad) throws Exception {
         if (cantidad < 0) {
@@ -38,6 +67,11 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Metódo retirar
+     * @param cantidad Parámetro de la cantidad que se va a retirar
+     * @throws Exception  si se cumple una condición dada dentro del metodo, mostrará un mensaje por pantalla.
+     */
     public void retirar(double cantidad) throws Exception {
         if (cantidad <= 0)
             throw new Exception("No se puede retirar una cantidad negativa");
@@ -46,34 +80,66 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
+    /**
+     * Metodo getter de nombre
+     * @return devulve el nombre del propietario de la cuenta
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Metodo setter de nombre
+     * @param nombre Otorga el nombre ingresado al atributo de la clase
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     *Metodo getter de el numero de cuenta
+     * @return devuelve el numero de cuenta
+     */
     public String getCuenta() {
         return cuenta;
     }
 
+    /**
+     * Metodo setter de cuenta
+     * @param cuenta Otorga una cadena de numero y guiones al atributo de la clase
+     */
     public void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
+    /**
+     * Metodo getter de saldo
+     * @return devuelve el saldo de la cuenta
+     */
     public double getSaldo() {
         return saldo;
     }
 
+    /**
+     * Metodo setter del saldo
+     * @param saldo Otorga un saldo en numeros al atributo de la clase
+     */
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
+    /**
+     *Metodo getter del tipo de interes
+     * @return devuelve el tipo de interes
+     */
     public double getTipoInteres() {
         return tipoInteres;
     }
 
+    /**
+     * Metodo setter del tipo de interes
+     * @param tipoInteres Parámetro del tipo double que asigna un valor al atributo de la clase
+     */
     public void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
     }
